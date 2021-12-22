51eb34f2dceaf9af245092dfe347820c7f451904
michi <michiko@michimac.local>
2021-12-23 06:45:46 +0900

    css修正,php追加

---
+++ submit.php
@@ -0,0 +1,7 @@
+<h2>送信完了</h2>
+<p class="message">お問い合わせありがとうございます。
+自動返信メールをお送りしておりますのでご確認ください。
+</p>
+<form action="#"> <!-- //ホームのURL -->
+<input id="title" name="submit" type="submit" value="ホームに戻る" />
+</form>
